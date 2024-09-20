8aa8cb2218daf7b706faa9ae155972c678c6d3b3
Mohamed-Ahnaf <ahnafmohamed1210@gmail.com>
2024-09-21 02:54:50 +0530

    updated more

--- script1.js
+++ script1.js
@@ -1,6 +1,14 @@
-const btn=document.getElementsByClassName('btn')[0];
-const navbar=document.getElementsByClassName('navbar-links')[0];
+const btn = document.getElementsByClassName('btn')[0];
+const navbar = document.getElementsByClassName('navbar-links')[0];
+const navLinks = navbar.getElementsByTagName('a'); // Assuming your links are <a> elements
 
-btn.addEventListener('click',()=>{
-navbar.classList.toggle('active');
+btn.addEventListener('click', () => {
+    navbar.classList.toggle('active');
 });
+
+// Close navbar when a link is clicked
+for (let link of navLinks) {
+    link.addEventListener('click', () => {
+        navbar.classList.remove('active');
+    });
+}
